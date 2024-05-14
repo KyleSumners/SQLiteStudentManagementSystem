4efd95c995bd9d20b9a9ccbd1bf634695e547bdd
KyleSumners <ksumners66@gmail.com>
2024-05-14 12:51:55 -0500

    Implement searching for a student in records

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, \
     QTableWidgetItem, QDialog, QVBoxLayout, QLineEdit, QComboBox, \
-    QPushButton
+    QPushButton, QToolBar
 from PyQt6.QtGui import QAction
+from PyQt6.QtCore import Qt
 import sys
 import sqlite3
 
@@ -24,7 +25,7 @@ class MainWindow(QMainWindow):
 
         search_student_action = QAction("Search", self)
         search_student_action.triggered.connect(self.search)
-        edit_menu_item.addAction(search_student_action)
+        edit_menu_item.addAction(search_student_action)\
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
@@ -32,6 +33,7 @@ class MainWindow(QMainWindow):
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
         self.load_data()
+
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
@@ -98,6 +100,40 @@ class InsertDialog(QDialog):
         cursor.close()
         connection.close()
         main_window.load_data()
+
+
+class SearchDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Search Student")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Create student name search widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Create button
+        button = QPushButton("Search")
+        button.clicked.connect(self.search)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def search(self):
+        name = self.student_name.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+        items = main_window.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            main_window.table.item(item.row(), 1).setSelected(True)
+
+        cursor.close()
+        connection.close()
 
 
 
